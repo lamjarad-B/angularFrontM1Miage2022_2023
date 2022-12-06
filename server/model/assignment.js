de7ba4c2a3c21816e2b539934ba34a4c6240a941
Lamjarad Brahim <52073076+Agares1990@users.noreply.github.com>
2022-12-06 16:13:55 +0100

    Séparation de la partie front et back-end

---
+++ server/model/assignment.js
@@ -0,0 +1,13 @@
+let mongoose = require('mongoose');
+var aggregatePaginate = require("mongoose-aggregate-paginate-v2");
+let Schema = mongoose.Schema;
+
+let AssignmentSchema = Schema({
+    id: Number,
+    dateDeRendu: Date,
+    nom: String,
+    rendu: Boolean
+});
+AssignmentSchema.plugin(aggregatePaginate);
+// C'est à travers ce modèle Mongoose qu'on pourra faire le CRUD
+module.exports = mongoose.model('Assignment', AssignmentSchema);
