a963176c4da0ba7469d3311550f376cde6a644a5
Lamjarad Brahim <52073076+Agares1990@users.noreply.github.com>
2022-12-29 23:28:18 +0100

    Ajout du formulaire d'étapes dans l'édition des devoirs

--- client/src/app/assignments/edit-assignment/edit-assignment.component.ts
+++ client/src/app/assignments/edit-assignment/edit-assignment.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute, Router } from "@angular/router";
+import { FormBuilder, Validators } from "@angular/forms";
 import { AssignmentsService } from "src/app/shared/assignments.service";
 import { CoursesService } from "src/app/shared/courses.service";
 import { Assignment } from "../../models/assignment.model";
@@ -15,15 +16,33 @@ export class EditAssignmentComponent implements OnInit
 {
 	// Propriétés du composant.
 	assignment!: Assignment | undefined;
-	nomAssignment!: string;
-	dateDeRendu!: Date;
 	noteAssignment!: number;
 	teacher: string = "Inconnu";
 	image!: string;
 	courses: Course[] = [];
 
+	// Propriétés pour le formulaire.
+	firstFormGroup = this._formBuilder.group( {
+		firstCtrl: [ "", Validators.required ], // Nom du devoir.
+	} );
+
+	secondFormGroup = this._formBuilder.group( {
+		secondCtrl: [ "", Validators.required ], // Matière du devoir.
+	} );
+
+	thirdFormGroup = this._formBuilder.group( {
+		thirdCtrl: [ "", Validators.required ], // Date de rendu.
+	} );
+
+	fourthFormGroup = this._formBuilder.group( {
+		fourthCtrl: [ "", Validators.required ], // Note du devoir.
+	} );
+
+	isLinear = true;
+
 	// Constructeur.
 	constructor(
+		private _formBuilder: FormBuilder,
 		private assignmentsService: AssignmentsService,
 		private coursesService: CoursesService,
 		private route: ActivatedRoute,
@@ -34,18 +53,17 @@ export class EditAssignmentComponent implements OnInit
 	ngOnInit(): void
 	{
 		// Récupération de l'identifiant dans l'URL.
-		const id = +this.route.snapshot.params[ "id" ];
-
-		this.assignmentsService.getAssignment( id ).subscribe( ( assignment ) =>
+		this.assignmentsService.getAssignment( +this.route.snapshot.params[ "id" ] ).subscribe( ( assignment ) =>
 		{
 			// Si l'identifiant n'existe pas, on retourne rien.
 			if ( !assignment ) return;
 
 			// On stocke le devoir dans la propriété du composant.
 			this.assignment = assignment;
-			this.nomAssignment = assignment.nom;
-			this.noteAssignment = assignment.note;
-			this.dateDeRendu = assignment.dateDeRendu;
+			this.firstFormGroup.setValue( { firstCtrl: assignment.nom } );
+			this.secondFormGroup.setValue( { secondCtrl: assignment.course.toString() } );
+			this.thirdFormGroup.setValue( { thirdCtrl: assignment.dateDeRendu.toString() } );
+			this.fourthFormGroup.setValue( { fourthCtrl: assignment.note.toString() } );
 		} );
 
 		// Récupération de toutes les matières (avec pagination).
@@ -70,13 +88,17 @@ export class EditAssignmentComponent implements OnInit
 	// Méthode pour sauvegarder les modifications d'un devoir.
 	onSaveAssignment()
 	{
-		// si le devoir n'existe pas, on retourne rien.
-		if ( !this.assignment ) return;
+		// Si le devoir n'existe pas ou si le formulaire n'est pas valide, on ne fait rien.
+		if ( !this.assignment || !this.firstFormGroup.value.firstCtrl || !this.secondFormGroup.value.secondCtrl || !this.thirdFormGroup.value.thirdCtrl || !this.fourthFormGroup.value.fourthCtrl )
+		{
+			return;
+		}
 
-		// on récupère les valeurs dans le formulaire.
-		this.assignment.nom = this.nomAssignment;
-		this.assignment.note = Math.min( Math.max( this.noteAssignment, 0 ), 20 );
-		this.assignment.dateDeRendu = this.dateDeRendu;
+		// On récupère alors les informations du formulaire.
+		this.assignment.nom = this.firstFormGroup.value.firstCtrl;
+		this.assignment.course = +this.secondFormGroup.value.secondCtrl;
+		this.assignment.dateDeRendu = new Date( this.thirdFormGroup.value.thirdCtrl );
+		this.assignment.note = Math.min( Math.max( +this.fourthFormGroup.value.fourthCtrl, 0 ), 20 );
 		this.assignmentsService
 			.updateAssignment( this.assignment )
 			.subscribe( () =>
